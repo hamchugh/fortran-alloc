124f2f34fcb44a892a20d198cab2080f680f5719
Harry McHugh <hamchugh@amd.com>
2023-12-21 10:39:13 +0000

    Genoa run

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Use Spack for compiler access
-. /home/hamchugh/spack/share/spack/setup-env.sh
+. /work/hamchugh/spack/share/spack/setup-env.sh
 
 # Clean up old data
 rm -rf ./*.exe
@@ -22,7 +22,7 @@ numactl --physcpubind=0 time ./flang4.1.0_toy.exe
 # Remove previously used compilers
 spack unload --all
 # Make intel compiler available
-spack load intel-oneapi-compilers
+spack load intel-oneapi-compilers target=x86_64
 export FC=ifort
 export FFLAGS="-O3 -g"
 export PROGRAM=ifort2021.10.0_toy.exe
@@ -32,9 +32,10 @@ numactl --physcpubind=0 time ./ifort2021.10.0_toy.exe
 
 # Remove previously used compilers
 spack unload --all
+spack load gcc@13.1.0 target=x86_64
 export FC=gfortran
 export FFLAGS="-O3 -g"
-export PROGRAM=gfortran11.3.0_toy.exe
+export PROGRAM=gfortran13.1.0_toy.exe
 make clean && make
-numactl --physcpubind=0 perf record -o perf.data.gfortran11.3.0 ./gfortran11.3.0_toy.exe
-numactl --physcpubind=0 time ./gfortran11.3.0_toy.exe
+numactl --physcpubind=0 perf record -o perf.data.gfortran13.1.0 ./gfortran13.1.0_toy.exe
+numactl --physcpubind=0 time ./gfortran13.1.0_toy.exe
