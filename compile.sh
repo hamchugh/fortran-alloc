06229ce152b6fdae3621c86a977759010be89c22
Harry McHugh <hamchugh@amd.com>
2023-12-28 14:37:30 +0000

    Tidy up

--- compile.sh
+++ compile.sh
@@ -15,11 +15,12 @@ spack load aocc@4.1.0 target=x86_64
 export FC=flang
 export FFLAGS="-O3 -Mstack_arrays -g -march=native -mtune=native"
 export LDFLAGS="-lm -lamdalloc"
-export MALLOC_CONF=stats_print:true
 export PROGRAM=flang4.1.0_toy.exe
 make clean && make
 numactl --physcpubind=0 perf stat ./flang4.1.0_toy.exe
 numactl --physcpubind=0 perf record -o perf.data.flang4.1.0 ./flang4.1.0_toy.exe
+echo "TIMING FOR ZENSS"
+export MALLOC_CONF=stats_print:true
 numactl --physcpubind=0 time ./flang4.1.0_toy.exe
 
 # Remove previously used compilers
@@ -33,6 +34,7 @@ export PROGRAM=ifort2021.10.0_toy.exe
 make clean && make
 numactl --physcpubind=0 perf stat ./ifort2021.10.0_toy.exe
 numactl --physcpubind=0 perf record -o perf.data.ifort2021.10.0 ./ifort2021.10.0_toy.exe
+echo "TIMING FOR INTEL"
 numactl --physcpubind=0 time ./ifort2021.10.0_toy.exe
 
 # Remove previously used compilers
@@ -45,4 +47,5 @@ export PROGRAM=gfortran13.1.0_toy.exe
 make clean && make
 numactl --physcpubind=0 perf stat ./gfortran13.1.0_toy.exe
 numactl --physcpubind=0 perf record -o perf.data.gfortran13.1.0 ./gfortran13.1.0_toy.exe
+echo "TIMING FOR GNU"
 numactl --physcpubind=0 time ./gfortran13.1.0_toy.exe
