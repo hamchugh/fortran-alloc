bac40173c7cbb33dbbdcbfcaeb11c41425760d84
Harry McHugh <hamchugh@amd.com>
2024-01-03 11:45:35 +0000

    remove znver dirs

--- compile.sh
+++ compile.sh
@@ -26,7 +26,7 @@ export LDFLAGS="-lm -lamdalloc"
 export PROGRAM=flang4.1.0_toy.exe
 make clean && make
 numactl --physcpubind=0 perf stat ./flang4.1.0_toy.exe
-numactl --physcpubind=0 perf record -o perf.data.flang4.1.0 ./flang4.1.0_toy.exe
+numactl --physcpubind=0 perf record --call-graph dwarf -o perf.data.flang4.1.0 ./flang4.1.0_toy.exe
 echo "TIMING FOR ZENSS"
 export MALLOC_CONF=stats_print:true
 numactl --physcpubind=0 time ./flang4.1.0_toy.exe
@@ -52,7 +52,7 @@ export LDFLAGS="-lm"
 export PROGRAM=ifort2021.10.0_toy.exe
 make clean && make
 numactl --physcpubind=0 perf stat ./ifort2021.10.0_toy.exe
-numactl --physcpubind=0 perf record -o perf.data.ifort2021.10.0 ./ifort2021.10.0_toy.exe
+numactl --physcpubind=0 perf record --call-graph dwarf -o perf.data.ifort2021.10.0 ./ifort2021.10.0_toy.exe
 echo "TIMING FOR INTEL"
 numactl --physcpubind=0 time ./ifort2021.10.0_toy.exe
 
@@ -65,7 +65,7 @@ export LDFLAGS="-lm"
 export PROGRAM=gfortran13_toy.exe
 make clean && make
 numactl --physcpubind=0 perf stat ./gfortran13_toy.exe
-numactl --physcpubind=0 perf record -o perf.data.gfortran13 ./gfortran13_toy.exe
+numactl --physcpubind=0 perf record --call-graph dwarf -o perf.data.gfortran13 ./gfortran13_toy.exe
 echo "TIMING FOR GNU"
 numactl --physcpubind=0 time ./gfortran13_toy.exe
 
